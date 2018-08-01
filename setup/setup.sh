914b49e669219c20700892d67043215385426de9
Benjamin Keil <privateblockchainsolutions@gmail.com>
2018-08-01 13:08:49 +0100

    fixing nvm path mistake and script re-naming

--- setup/setup.sh
+++ setup/setup.sh
@@ -29,9 +29,9 @@ sudo apt-get -y install python
 echo "[*] Installing NVM and nodejs 8 "
 sleep 2
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh
-chmod +x install.#!/bin/sh
-./install.sh
+chmod +x install.sh
 
+./install.sh
 . ~/.bashrc
 nvm install 8
 nvm use 8
